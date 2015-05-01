6f2028f21d43a170375870531029ae1b5199da3b
butterfly <mengdiehu0510@gmail.com>
2015-05-01 13:57:07 -0400

    format

--- cell_gpu.cu
+++ cell_gpu.cu
@@ -5,81 +5,91 @@
 
 // the kernel to udpate the state of each cell based on its neighbors for certain number of steps
 __global__ void update(int *in, int *out, int dim){
-
-	int x = threadIdx.x + blockIdx.x * blockDim.x;
-	int y = threadIdx.y + blockIdx.y * blockDim.y;
-	int offset = x + y * blockDim.x * gridDim.x;
-	while(offset < dim*dim){
+    
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int offset = x + y * blockDim.x * gridDim.x;
+    while (offset < dim * dim) {
         int sum = 0;
-	for(int i=-1; i<2; i++) {
-		for(int j=-1; j<2; j++) {
-			int xtemp=(x+i+dim)%dim;
-			int ytemp=(y+j+dim)%dim;
-			int offsettemp=xtemp + ytemp*blockDim.x*gridDim.x;
-			sum=sum+in[offsettemp];
-		}
-	}
-	sum=sum-in[offset];
-	if (in[offset] == 1) {
-		if(sum == 2 || sum ==3) out[offset] = 1;
-		else out[offset] = 0;
-	}else{
-	if( sum == 3) out[offset] = 1;
-        else out[offset] = 0;
-	}
-        offset = offset + blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+        for(int i=-1; i < 2; i++) {
+            for(int j=-1; j < 2; j++) {
+                int xtemp = (x + i + dim) % dim;
+                int ytemp = (y + j + dim) % dim;
+                int offsettemp = xtemp + ytemp * blockDim.x * gridDim.x;
+                sum = sum + in[offsettemp];
+            }
         }
-
+        sum = sum - in[offset];
+        if (in[offset] == 1) {
+            if (sum == 2 || sum ==3) { 
+                out[offset] = 1;
+            }
+            else { 
+                out[offset] = 0;
+            }
+        }
+        else {
+            if( sum == 3) {
+                out[offset] = 1;
+            }
+            else {
+                out[offset] = 0;
+            }
+        }
+        offset = offset + blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+    }
 }
-	
-int main(int argc, char *argv[]){
+
+int main(int argc, char *argv[]) {
 
     clock_t start;
-	int dim=2<<8;
-	int nStep=2<<8;
-	int frequency=2<<8;
-	int size=dim*dim;
-	int step;
-	DataBlock data;
-	data.bitmap=(int *)malloc(size*sizeof(int));
-        for (int i=0; i<size; i++) {
-		data.bitmap[i] = 0;
-	}
-	data.bitmap[1]=1;
-        data.bitmap[dim+2] = 1;
-        data.bitmap[2 * dim + 0] = 1;
-        data.bitmap[2 * dim + 1] = 1;
-        data.bitmap[2 * dim + 2] = 1;
-	data.outbitmap=(int *)malloc(size*sizeof(int));
-        int bitmapSize=size*sizeof(int);
-
-	start=clock();
-	HANDLE_ERROR(cudaMalloc( (void **)&(data.dev_in),bitmapSize));
-	HANDLE_ERROR(cudaMalloc( (void **)&(data.dev_out),bitmapSize));
-	
-
-
-
-	HANDLE_ERROR(cudaMemcpy(data.dev_in, data.bitmap, bitmapSize, cudaMemcpyHostToDevice));
-
-	dim3 dimgrid(dim/16,dim/16);
-	dim3 dimblock(16,16);
-
-	for(step = 0; step<nStep; step++ ){
-	update<<<dimgrid,dimblock>>>(data.dev_in,data.dev_out,dim);
-	
-	swap(data.dev_in,data.dev_out);
-	if(step % frequency == 0 ){
-	HANDLE_ERROR(cudaMemcpy(data.outbitmap,data.dev_out,bitmapSize,cudaMemcpyDeviceToHost));
-      printf ( "\nIteration %d: final grid:\n", step );
-      for ( int j = 0; j < size; j++ ) {
-        if ( j % dim == 0 ) {printf( "\n" );}
-        printf ( "%d", data.outbitmap[j] );
-      }
-      printf( "\n" );
-	}
-	}
-	HANDLE_ERROR(cudaFree(data.dev_in));
-	HANDLE_ERROR(cudaFree(data.dev_out));
-    printf("%d\n",clock()-start);
+    int dim = 2<<8;
+    int nStep = 2<<8;
+    int frequency = 2<<8;
+    int size = dim * dim;
+    int step;
+    DataBlock data;
+    data.bitmap=(int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        data.bitmap[i] = 0;
+    }
+    data.bitmap[1]=1;
+    data.bitmap[dim+2] = 1;
+    data.bitmap[2 * dim + 0] = 1;
+    data.bitmap[2 * dim + 1] = 1;
+    data.bitmap[2 * dim + 2] = 1;
+    data.outbitmap=(int *)malloc(size * sizeof(int));
+    int bitmapSize=size * sizeof(int);
+    
+    start=clock();
+    HANDLE_ERROR(cudaMalloc( (void **)&(data.dev_in), bitmapSize));
+    HANDLE_ERROR(cudaMalloc( (void **)&(data.dev_out), bitmapSize));
+    
+    
+    
+    
+    HANDLE_ERROR(cudaMemcpy(data.dev_in, data.bitmap, bitmapSize, cudaMemcpyHostToDevice));
+    
+    dim3 dimgrid(dim / 16, dim / 16);
+    dim3 dimblock(16, 16);
+    
+    for(step = 0; step < nStep; step++ ){
+        update<<<dimgrid,dimblock>>>(data.dev_in, data.dev_out,dim);
+        
+        swap(data.dev_in,data.dev_out);
+        if(step % frequency == 0 ){
+            HANDLE_ERROR(cudaMemcpy(data.outbitmap, data.dev_out, bitmapSize, cudaMemcpyDeviceToHost));
+            printf ( "\nIteration %d: final grid:\n", step );
+            for (int j = 0; j < size; j++) {
+                if ( j % dim == 0 ) {
+                    printf( "\n" );
+                }
+                printf("%d", data.outbitmap[j]);
+            }
+            printf( "\n" );
+        }
+    }
+    HANDLE_ERROR(cudaFree(data.dev_in));
+    HANDLE_ERROR(cudaFree(data.dev_out));
+    printf("%d\n", clock() - start);
 }
