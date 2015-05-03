41a2a070f3125976c7433dadf2326e5e56f40655
Mengtie Hu <mdhu@ugrad1.cs.jhu.edu>
2015-05-03 00:51:56 -0400

    fix some output format problem

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ if [ -e $PATH ]; then
 	exit 1
 fi
 
-for dim in {6..7}; do
+for dim in {6..12}; do
 	for freq in {0..10}; do
 		timeval=$(./cell_cpu $[2**$dim] 1024 $[2**freq])
 		echo $timeval >> $PATH
@@ -38,33 +38,33 @@ for dim in {6..12}; do
 done
 
 #------------------ gpu: tex1d ------------------
-nvcc cell_gpu_tex1d.cu -o cell_gpu_tex1d
-PATH="$DIR/gpu_tex1d.log"
-if [ -e $PATH ]; then
-	echo "File $PATH already exists, please delete or rename it first!" >&2
-	exit 1
-fi
-
-for dim in {6..12}; do
-	for freq in {0..10}; do
-		timeval=$(./cell_gpu_tex1d $[2**$dim] 1024 $[2**freq])
-		echo $timeval >> $PATH
-	done
-	printf "\n" >> $PATH
-done
-
-#------------------ gpu: tex2d ------------------
-nvcc cell_gpu_tex2d.cu -o cell_gpu_tex2d
-PATH="$DIR/gpu_tex2d.log"
-if [ -e $PATH ]; then
-	echo "File $PATH already exists, please delete or rename it first!" >&2
-	exit 1
-fi
-
-for dim in {6..12}; do
-	for freq in {0..10}; do
-		timeval=$(./cell_gpu_tex2d $[2**$dim] 1024 $[2**freq])
-		echo $timeval >> $PATH
-	done
-	printf "\n" >> $PATH
-done
+# nvcc cell_gpu_tex1d.cu -o cell_gpu_tex1d
+# PATH="$DIR/gpu_tex1d.log"
+# if [ -e $PATH ]; then
+# 	echo "File $PATH already exists, please delete or rename it first!" >&2
+# 	exit 1
+# fi
+# 
+# for dim in {6..12}; do
+# 	for freq in {0..10}; do
+# 		timeval=$(./cell_gpu_tex1d $[2**$dim] 1024 $[2**freq])
+# 		echo $timeval >> $PATH
+# 	done
+# 	printf "\n" >> $PATH
+# done
+# 
+# #------------------ gpu: tex2d ------------------
+# nvcc cell_gpu_tex2d.cu -o cell_gpu_tex2d
+# PATH="$DIR/gpu_tex2d.log"
+# if [ -e $PATH ]; then
+# 	echo "File $PATH already exists, please delete or rename it first!" >&2
+# 	exit 1
+# fi
+# 
+# for dim in {6..12}; do
+# 	for freq in {0..10}; do
+# 		timeval=$(./cell_gpu_tex2d $[2**$dim] 1024 $[2**freq])
+# 		echo $timeval >> $PATH
+# 	done
+# 	printf "\n" >> $PATH
+# done
