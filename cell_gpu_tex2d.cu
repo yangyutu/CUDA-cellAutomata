56ef176ce13e828a4e7ea46755d409cef70d5a74
yuguang yang <yangyutu123@gmail.com>
2015-05-04 18:01:38 -0400

    g

--- cell_gpu_tex2d.cu
+++ cell_gpu_tex2d.cu
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]){
     clock_t start;
     clock_t gpu_start;
  float gpu_comp_time = 0;
-    float gpu_mem_time = 0;
+    float gpu_mem_to_time = 0, gpu_mem_back_time=0;
 
 	int dim = atoi(argv[1]);
 	int nStep = atoi(argv[2]);
@@ -88,11 +88,11 @@ int main(int argc, char *argv[]){
 
 	HANDLE_ERROR(cudaMemcpy(data.dev_in, data.bitmap, bitmapSize, cudaMemcpyHostToDevice));
 
- gpu_mem_time += ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
+ gpu_mem_to_time = ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
 
 	dim3 dimgrid(dim / 16, dim / 16);
 	dim3 dimblock(16, 16);
-
+ gpu_start = clock();
 	flag = 1;
 	for(step = 0; step < nStep; step++ ){
 		int *in,*out;
@@ -103,28 +103,32 @@ int main(int argc, char *argv[]){
 		else {
 			out=data.dev_in; flag = 1;
 		}
- gpu_start = clock();
+
 		update_tex1d<<<dimgrid,dimblock>>>(out,flag,dim);
-		  gpu_comp_time += ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
+	
 //		swap(data.dev_in,data.dev_out);
 
-		if(step % frequency == frequency - 1){
-  gpu_start = clock();
-			HANDLE_ERROR(cudaMemcpy(data.outbitmap,data.dev_out,bitmapSize,cudaMemcpyDeviceToHost));
-  gpu_mem_time += ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
-			printf ("\nIteration %d / %d : \n", step + 1, nStep);
-			for ( int j = 0; j < size; j++ ) {
-				if (j % dim == 0) {
-					printf( "\n" );
-				}
-				printf ("%d  ", data.outbitmap[j]);
-			}
-			printf("\n");
-		}
+//		if(step % frequency == frequency - 1){
+//  gpu_start = clock();
+//			HANDLE_ERROR(cudaMemcpy(data.outbitmap,data.dev_out,bitmapSize,cudaMemcpyDeviceToHost));
+//  gpu_mem_time += ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
+//			printf ("\nIteration %d / %d : \n", step + 1, nStep);
+//			for ( int j = 0; j < size; j++ ) {
+//				if (j % dim == 0) {
+//					printf( "\n" );
+//				}
+//				printf ("%d  ", data.outbitmap[j]);
+//			}
+//			printf("\n");
+//		}
     }
-
+	cudaDeviceSynchronize();
+	  gpu_comp_time += ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
+ gpu_start = clock();
+ gpu_mem_back_time = ((float)(clock() - gpu_start)) / CLOCKS_PER_SEC;
 	HANDLE_ERROR(cudaFree(data.dev_in));
 	HANDLE_ERROR(cudaFree(data.dev_out));
-  printf("%f %f ", gpu_comp_time, gpu_mem_time);
+
+    printf("%f %f %f ", gpu_comp_time, gpu_mem_to_time, gpu_mem_back_time);
     printf("%f\n", ((float)(clock() - start)) / CLOCKS_PER_SEC);
 }
