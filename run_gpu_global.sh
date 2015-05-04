a42be4c88fac621c85da42cfea09d0325bfdd2ab
Yang Yuguang DY 600.476 F15 <yyang60@ugrad1.cs.jhu.edu>
2015-05-04 17:31:35 -0400

    g

---
+++ run_gpu_global.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+DIR="logfiles"
+if [ ! -e $DIR ]; then
+	mkdir $DIR
+fi
+
+#----------------- gpu: global ------------------
+nvcc cell_gpu.cu -o cell_gpu
+PATH="$DIR/gpu_global.log"
+if [ -e $PATH ]; then
+	echo "File $PATH already exists, please delete or rename it first!" >&2
+	exit 1
+fi
+
+for dim in {6..12}; do
+	timeval=$(./cell_gpu $[2**$dim] 1024 1)
+	echo $timeval >> $PATH
+done
+
+#------------------ gpu: tex1d ------------------
+# nvcc cell_gpu_tex1d.cu -o cell_gpu_tex1d
+# PATH="$DIR/gpu_tex1d.log"
+# if [ -e $PATH ]; then
+# 	echo "File $PATH already exists, please delete or rename it first!" >&2
+# 	exit 1
+# fi
+# 
+# for dim in {6..12}; do
+# 	for freq in {0..10}; do
+# 		timeval=$(./cell_gpu_tex1d $[2**$dim] 1024 $[2**freq])
+# 		echo $timeval >> $PATH
+# 	done
+# 	printf "\n" >> $PATH
+# done
+# 
+# #------------------ gpu: tex2d ------------------
+# nvcc cell_gpu_tex2d.cu -o cell_gpu_tex2d
+# PATH="$DIR/gpu_tex2d.log"
+# if [ -e $PATH ]; then
+# 	echo "File $PATH already exists, please delete or rename it first!" >&2
+# 	exit 1
+# fi
+# 
+# for dim in {6..12}; do
+# 	for freq in {0..10}; do
+# 		timeval=$(./cell_gpu_tex2d $[2**$dim] 1024 $[2**freq])
+# 		echo $timeval >> $PATH
+# 	done
+# 	printf "\n" >> $PATH
+# done
